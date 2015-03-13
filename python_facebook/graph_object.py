e1c9cf7fb13d97d4daa68210d0637defae075379
Karl WnW <karl.wnw@gmail.com>
2015-03-13 13:55:07 +0100

    Update GraphObject to handle the response['data'] case
    Sometimes the facebook response is a dict with the right keys at the 1st level,
    sometimes the relevant data is in the 'data' key.

--- python_facebook/graph_object.py
+++ python_facebook/graph_object.py
@@ -2,7 +2,15 @@
 
 
 class GraphObject(object):
+    """
+    Creates a GraphObject using the data provided
+    """
 
     def __init__(self, *args, **kwargs):
         self.backing_data = kwargs
-        self.__dict__.update(kwargs)
+
+        if self.backing_data.get('data') and len(self.backing_data) == 1:
+            self.backing_data = self.backing_data['data']
+
+        for key, value in self.backing_data.items():
+            setattr(self, key, value)
