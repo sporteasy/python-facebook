b9ffba07ab6dd187906ca4abbe4008c6384d504e
shostelet <simon.hostelet@gmail.com>
2017-07-17 16:48:49 +0200

    Implemented FacebookUrlManipulator

---
+++ python_facebook/sdk/url/facebook_url_manipulator.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import re
+import urlparse
+import urllib
+from python_facebook.sdk.response import QueryStringDictFormatter
+
+
+class FacebookUrlManipulator(object):
+
+    @staticmethod
+    def remove_params_from_url(url, params_to_filter):
+        parsed = urlparse.urlparse(url)
+        params = {}
+        if parsed.query:
+            for key, value in urlparse.parse_qs(parsed.query).items():
+                if key not in params_to_filter:
+                    params[key] = value[0]
+        port = '' if not parsed.port else ':{}'.format(parsed.port)
+        url = '{scheme}://{host}{port}{path}'.format(scheme=parsed.scheme, host=parsed.hostname,
+                                                     port=port, path=parsed.path)
+        return FacebookUrlManipulator.append_params_to_url(url, params)
+
+    @staticmethod
+    def append_params_to_url(url, new_params=None):
+        if not new_params:
+            return url
+
+        if '?' not in url:
+            return url + '?' + urllib.urlencode(sorted(new_params.items()))
+
+        path, query_string = url.split('?')
+        query_params = QueryStringDictFormatter(urlparse.parse_qs(query_string)).output
+        # Favor query_params from the original URL over params
+        new_params.update(query_params)
+
+        return path + '?' + urllib.urlencode(sorted(new_params.items()))
+
+    @staticmethod
+    def get_params_as_array(url):
+        parsed = urlparse.urlparse(url)
+        parsed_dict = urlparse.parse_qs(parsed.query)
+        return {key: value[0] for key, value in parsed_dict.items()}
+
+    @staticmethod
+    def merge_url_params(url_to_steal_from, url_to_add_to):
+        new_params = FacebookUrlManipulator.get_params_as_array(url_to_steal_from)
+        # Nothing new to add, return as- is
+        if not new_params:
+            return url_to_add_to
+        return FacebookUrlManipulator.append_params_to_url(url_to_add_to, new_params)
+
+    @staticmethod
+    def force_slash_prefix(string):
+        if not string or string[0] == '/':
+            return string
+        return '/{}'.format(string)
+
+    @staticmethod
+    def base_graph_url_endpoint(url_to_trim):
+        pattern = '^https?://.+\.facebook\.com(\/v.+?)?/'
+        return '/{}'.format(re.sub(pattern, '', url_to_trim))
---
+++ tests/url/test_facebook_url_manipulator.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import unittest
+
+from unittest_data_provider import data_provider
+
+from python_facebook.sdk.url.facebook_url_manipulator import FacebookUrlManipulator
+
+
+class TestFacebookUrlManipulator(unittest.TestCase):
+
+    dirty_urls = lambda: (
+        ('http://localhost/something?state=0000&foo=bar&code=abcd', 'http://localhost/something?foo=bar'),
+        ('https://localhost/something?state=0000&foo=bar&code=abcd', 'https://localhost/something?foo=bar'),
+        ('http://localhost/something?state=0000&foo=bar&error=abcd&error_reason=abcd&error_description=abcd'
+         '&error_code=1', 'http://localhost/something?foo=bar'),
+        ('https://localhost/something?state=0000&foo=bar&error=abcd&error_reason=abcd&error_description=abcd'
+         '&error_code=1', 'https://localhost/something?foo=bar'),
+        ('http://localhost/something?state=0000&foo=bar&error=abcd', 'http://localhost/something?foo=bar'),
+        ('https://localhost/something?state=0000&foo=bar&error=abcd', 'https://localhost/something?foo=bar'),
+        ('https://localhost:1337/something?state=0000&foo=bar&error=abcd', 'https://localhost:1337/something?foo=bar'),
+        ('https://localhost:1337/something?state=0000&code=foo', 'https://localhost:1337/something'),
+        ('https://localhost/something/?state=0000&code=foo&foo=bar', 'https://localhost/something/?foo=bar'),
+        ('https://localhost/something/?state=0000&code=foo', 'https://localhost/something/'),
+    )
+
+    @data_provider(dirty_urls)
+    def test_params_get_removed_from_a_url(self, dirty_url, expected_clean_url):
+        remove_params = [
+            'state',
+            'code',
+            'error',
+            'error_reason',
+            'error_description',
+            'error_code',
+        ]
+        current_uri = FacebookUrlManipulator.remove_params_from_url(dirty_url, remove_params)
+        self.assertEqual(expected_clean_url, current_uri)
+
+    def test_gracefully_handles_url_appending(self):
+        params = {}
+        url = 'https://www.foo.com/'
+        processed_url = FacebookUrlManipulator.append_params_to_url(url, params)
+        self.assertEqual('https://www.foo.com/', processed_url)
+
+        params = {'access_token': 'foo'}
+        url = 'https://www.foo.com/'
+        processed_url = FacebookUrlManipulator.append_params_to_url(url, params)
+        self.assertEqual('https://www.foo.com/?access_token=foo', processed_url)
+
+        params = {
+            'access_token': 'foo',
+            'bar': 'baz'
+        }
+        url = 'https://www.foo.com/?foo=bar'
+        processed_url = FacebookUrlManipulator.append_params_to_url(url, params)
+        self.assertEqual('https://www.foo.com/?access_token=foo&bar=baz&foo=bar', processed_url)
+
+        params = {
+            'access_token': 'foo',
+        }
+        url = 'https://www.foo.com/?foo=bar&access_token=bar'
+        processed_url = FacebookUrlManipulator.append_params_to_url(url, params)
+        self.assertEqual('https://www.foo.com/?access_token=bar&foo=bar', processed_url)
+
+    def test_slashes_are_properly_prepended(self):
+        slashTestOne = FacebookUrlManipulator.force_slash_prefix('foo')
+        slashTestTwo = FacebookUrlManipulator.force_slash_prefix('/foo')
+        slashTestThree = FacebookUrlManipulator.force_slash_prefix('foo/bar')
+        slashTestFour = FacebookUrlManipulator.force_slash_prefix('/foo/bar')
+        slashTestFive = FacebookUrlManipulator.force_slash_prefix(None)
+        slashTestSix = FacebookUrlManipulator.force_slash_prefix('')
+        self.assertEqual('/foo', slashTestOne)
+        self.assertEqual('/foo', slashTestTwo)
+        self.assertEqual('/foo/bar', slashTestThree)
+        self.assertEqual('/foo/bar', slashTestFour)
+        self.assertEqual(None, slashTestFive)
+        self.assertEqual('', slashTestSix)
+
+    def test_params_can_be_returned_as_array(self):
+        paramsOne = FacebookUrlManipulator.get_params_as_array('/foo')
+        paramsTwo = FacebookUrlManipulator.get_params_as_array('/foo?one=1&two=2')
+        paramsThree = FacebookUrlManipulator.get_params_as_array('https://www.foo.com')
+        paramsFour = FacebookUrlManipulator.get_params_as_array('https://www.foo.com/?')
+        paramsFive = FacebookUrlManipulator.get_params_as_array('https://www.foo.com/?foo=bar')
+        self.assertEqual({}, paramsOne)
+        self.assertEqual({'one': '1', 'two': '2'}, paramsTwo)
+        self.assertEqual({}, paramsThree)
+        self.assertEqual({}, paramsFour)
+        self.assertEqual({'foo': 'bar'}, paramsFive)
+
+    mergable_endpoints = lambda : (
+        (
+            'https://www.foo.com/?foo=ignore_foo&dance=fun',
+            '/me?foo=keep_foo',
+            '/me?dance=fun&foo=keep_foo',
+        ),
+        (
+            'https://www.bar.com?',
+            'https://foo.com?foo=bar',
+            'https://foo.com?foo=bar',
+        ),
+        (
+            'you',
+            'me',
+            'me',
+        ),
+        (
+            '/1234?swing=fun',
+            '/1337?bar=baz&west=coast',
+            '/1337?bar=baz&swing=fun&west=coast',
+        ),
+    )
+
+    @data_provider(mergable_endpoints)
+    def test_params_can_be_merged_onto_url_properly(self, url_one, url_two, expected):
+        result = FacebookUrlManipulator.merge_url_params(url_one, url_two)
+        self.assertEqual(result, expected)
+
+    def testGraphUrlsCanBeTrimmed(self):
+        full_graph_url = 'https://graph.facebook.com/'
+        base_graph_url = FacebookUrlManipulator.base_graph_url_endpoint(full_graph_url)
+        self.assertEqual('/', base_graph_url)
+
+        full_graph_url = 'https://graph.facebook.com/v1.0/'
+        base_graph_url = FacebookUrlManipulator.base_graph_url_endpoint(full_graph_url)
+        self.assertEqual('/', base_graph_url)
+
+        full_graph_url = 'https://graph.facebook.com/me'
+        base_graph_url = FacebookUrlManipulator.base_graph_url_endpoint(full_graph_url)
+        self.assertEqual('/me', base_graph_url)
+
+        full_graph_url = 'https://graph.beta.facebook.com/me'
+        base_graph_url = FacebookUrlManipulator.base_graph_url_endpoint(full_graph_url)
+        self.assertEqual('/me', base_graph_url)
+
+        full_graph_url = 'https://whatever-they-want.facebook.com/v2.1/me'
+        base_graph_url = FacebookUrlManipulator.base_graph_url_endpoint(full_graph_url)
+        self.assertEqual('/me', base_graph_url)
+
+        full_graph_url = 'https://graph.facebook.com/v5.301/1233?foo=bar'
+        base_graph_url = FacebookUrlManipulator.base_graph_url_endpoint(full_graph_url)
+        self.assertEqual('/1233?foo=bar', base_graph_url)
