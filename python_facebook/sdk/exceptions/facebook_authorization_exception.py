1ce35138d24af27db08c195173760ed63e9520ad
shostelet <simon.hostelet@gmail.com>
2017-07-17 11:09:49 +0200

    Created new exceptions files

---
+++ python_facebook/sdk/exceptions/facebook_authorization_exception.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from python_facebook.sdk.exceptions.facebook_sdk_exception import FacebookSDKException
+
+
+class FacebookAuthorizationException(FacebookSDKException):
+    pass
