6194f1b5e946a31312d6ba6c2a695019f9449365
shostelet <simon.hostelet@gmail.com>
2017-07-18 12:04:44 +0200

    New implementation of response

--- python_facebook/sdk/response.py
+++ python_facebook/sdk/response.py
@@ -28,63 +28,121 @@ class QueryStringDictFormatter(object):
 
 
 class FacebookResponse(object):
-
-    def __init__(self, request, body, raw_response, http_status_code=None, headers=None):
+    def __init__(self, request, body=None, http_status_code=None, headers=None):
         self.request = request
-        # self.response_data = QueryStringDictFormatter(body).output
         self.body = body
-        self.raw_response = raw_response
         self.http_status_code = http_status_code
-        self.headers = headers
+        self.headers = headers or {}
 
         self.decode_body()
 
-    # def get_graph_object(self, cls=None):
-    #     if cls is None:
-    #         cls = GraphObject
-    #     return cls(**self.response_data)
+    def get_request(self):
+        return self.request
 
-    def get_graph_node(self, subclass_name=None):
-        """
-        Instantiate a new GraphNode from response.
-        """
-        factory = GraphNodeFactory(self)
-        return factory.make_graph_node(subclass_name)
+    def get_app(self):
+        return self.request.get_app()
+
+    def get_access_token(self):
+        return self.request.get_access_token()
+
+    def get_http_status_code(self):
+        return self.http_status_code
+
+    def get_headers(self):
+        return self.headers
+
+    def get_body(self):
+        return self.body
 
     def get_decoded_body(self):
         return self.decoded_body
 
+    def get_app_secret_proof(self):
+        return self.request.get_app_secret_proof()
+
+    def get_e_tag(self):
+        return self.headers.get('ETag', None)
+
+    def get_graph_version(self):
+        return self.headers.get('Facebook-API-Version', None)
+
+    def is_error(self):
+        return 'error' in self.decoded_body
+
+    def throw_exception(self):
+        raise self.thrown_exception
+
+    def make_exception(self):
+        self.thrown_exception = FacebookResponseException.create(self)
+
+    def get_thrown_exception(self):
+        return self.thrown_exception
+
     def decode_body(self):
         """
-            Convert the raw response into an array if possible.
+        Convert the raw response into an array if possible.
 
-            Graph will return 2 types of responses:
-                - JSON(P)
-                  Most responses from Graph are JSON(P)
-                - application/x-www-form-urlencoded key/value pairs
-                  Happens on the `/oauth/access_token` endpoint when exchanging
-                  a short-lived access token for a long-lived access token
-                - And sometimes nothing :/ but that'd be a bug.
+        Graph will return 2 types of responses:
+        - JSON(P)
+           Most responses from Graph are JSON(P)
+        - application/x-www-form-urlencoded key/value pairs
+           Happens on the `/oauth/access_token` endpoint when exchanging
+           a short-lived access token for a long-lived access token
+        - And sometimes nothing :/ but that'd be a bug.
         """
         try:
             self.decoded_body = json.loads(self.body)
         except ValueError:
             self.decoded_body = {}
-
             try:
                 self.decoded_body = urlparse.parse_qs(self.body)
+                self.decoded_body = {key: val[0] for key, val in self.decoded_body.items()}
             except ValueError:
-                if is_number(self.body):
+                if is_bool(self.body):
+                    self.decoded_body = {'success': to_boolean(self.body)}
+                elif is_number(self.body):
                     self.decoded_body = {'id': self.body}
 
         if self.is_error():
             self.make_exception()
 
-    def is_error(self):
-        return self.decoded_body and 'error' in self.decoded_body
+    def get_graph_object(self, subclass_name=None):
+        return self.get_graph_node(subclass_name)
 
-    def make_exception(self):
-        self.thrown_exception = FacebookResponseException.create(self)
+    def get_graph_node(self, subclass_name=None):
+        factory = GraphNodeFactory(self)
+        return factory.make_graph_node(subclass_name)
+
+    def get_graph_album(self):
+        factory = GraphNodeFactory(self)
+        return factory.make_graph_album()
+
+    def get_graph_page(self):
+        factory = GraphNodeFactory(self)
+        return factory.make_graph_page()
+
+    def get_graph_session_info(self):
+        factory = GraphNodeFactory(self)
+        return factory.make_graph_session_info()
+
+    def get_graph_user(self):
+        factory = GraphNodeFactory(self)
+        return factory.make_graph_user()
+
+    def get_graph_event(self):
+        factory = GraphNodeFactory(self)
+        return factory.make_graph_event()
+
+    def get_graph_group(self):
+        factory = GraphNodeFactory(self)
+        return factory.make_graph_group()
+
+    def get_graph_list(self, subclass_name=None, auto_prefix=True):
+        return self.get_graph_edge(subclass_name, auto_prefix)
+
+    def get_graph_edge(self, subclass_name=None, auto_prefix=True):
+        factory = GraphNodeFactory(self)
+        return factory.make_graph_edge(subclass_name, auto_prefix)
 
 
 def is_number(s):
@@ -93,3 +151,12 @@ def is_number(s):
         return True
     except ValueError:
         return False
+
+
+def is_bool(s):
+    return s in (True, 'true', 'True', 1, '1', 'on', 'On',
+                 False, 'false', 'False', 0, '0', 'off', 'Off')
+
+
+def to_boolean(s):
+    return s in (True, 'true', 'True', 1, '1', 'on', 'On')
---
+++ tests/facebook_response_test.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import unittest
+
+from python_facebook.sdk.exceptions.facebook_response_exception import FacebookResponseException
+from python_facebook.sdk.graph_nodes.graph_node import GraphNode
+from python_facebook.sdk.response import FacebookResponse
+
+from python_facebook.sdk.facebook_app import FacebookApp
+from python_facebook.sdk.request import FacebookRequest
+
+
+class FacebookResponseTest(unittest.TestCase):
+    def setUp(self):
+        app = FacebookApp('123', 'foo_secret')
+        self.request = FacebookRequest(app, 'foo_token', method='GET', endpoint='/me/photos?keep=me',
+                                       params={'foo': 'bar'}, e_tag='foo_eTag', graph_version='v1337')
+
+    def testAnETagCanBeProperlyAccessed(self):
+        response = FacebookResponse(self.request, '', 200, {'ETag': 'foo_tag'})
+        eTag = response.get_e_tag()
+        self.assertEqual('foo_tag', eTag)
+
+    def testAProperAppSecretProofCanBeGenerated(self):
+        response = FacebookResponse(self.request)
+        appSecretProof = response.get_app_secret_proof()
+        self.assertEqual('df4256903ba4e23636cc142117aa632133d75c642bd2a68955be1443bd14deb9', appSecretProof)
+
+    def testASuccessfulJsonResponseWillBeDecodedToAGraphNode(self):
+        graphResponseJson = '{"id":"123","name":"Foo"}'
+        response = FacebookResponse(self.request, graphResponseJson, 200)
+        decodedResponse = response.get_decoded_body()
+        graphNode = response.get_graph_node()
+        self.assertFalse(response.is_error(), 'Did not expect Response to return an error.')
+        self.assertEqual({
+            'id': '123',
+            'name': 'Foo',
+        }, decodedResponse)
+        self.assertIsInstance(graphNode, GraphNode)
+
+    def testASuccessfulJsonResponseWillBeDecodedToAGraphEdge(self):
+        graphResponseJson = '{"data":[{"id":"123","name":"Foo"},{"id":"1337","name":"Bar"}]}'
+        response = FacebookResponse(self.request, graphResponseJson, 200)
+        graphEdge = response.get_graph_edge()
+        self.assertFalse(response.is_error(), 'Did not expect Response to return an error.')
+        self.assertIsInstance(graphEdge[0], GraphNode)
+        self.assertIsInstance(graphEdge[1], GraphNode)
+
+    def testASuccessfulUrlEncodedKeyValuePairResponseWillBeDecoded(self):
+        graphResponseKeyValuePairs = 'id=123&name=Foo'
+        response = FacebookResponse(self.request, graphResponseKeyValuePairs, 200)
+        decodedResponse = response.get_decoded_body()
+        self.assertFalse(response.is_error(), 'Did not expect Response to return an error.')
+        self.assertEqual({
+            'id': '123',
+            'name': 'Foo',
+        }, decodedResponse)
+
+    def testErrorStatusCanBeCheckedWhenAnErrorResponseIsReturned(self):
+        graphResponse = '{"error":{"message":"Foo error.","type":"OAuthException","code":190,"error_subcode":463}}'
+        response = FacebookResponse(self.request, graphResponse, 401)
+        exception = response.get_thrown_exception()
+        self.assertTrue(response.is_error(), 'Expected Response to return an error.')
+        self.assertIsInstance(exception, FacebookResponseException)
