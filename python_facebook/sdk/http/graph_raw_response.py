9124e0dbaa2d739cbcf485414ab1c94b1ba0bdd6
shostelet <simon.hostelet@gmail.com>
2017-07-18 09:01:11 +0200

    Added class http GraphRawResponse

---
+++ python_facebook/sdk/http/graph_raw_response.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import re
+
+
+class GraphRawResponse(object):
+
+    def __init__(self, headers, body, http_status_code=None):
+        self.headers = {}
+        self.http_response_code = None
+        if isinstance(http_status_code, (int, long)):
+            self.http_response_code = int(http_status_code)
+
+        if isinstance(headers, dict):
+            self.headers = headers
+        else:
+            self.__set_headers_from_string(headers)
+        self.body = body
+
+    def get_headers(self):
+        return self.headers
+
+    def get_body(self):
+        return self.body
+
+    def get_http_response_code(self):
+        return self.http_response_code
+
+    def set_http_response_code_from_header(self, raw_response_header):
+        # ex: HTTP/1.1 200 OK
+        pattern = '^HTTP/\d\.\d\s+(\d+)\s+.*'
+
+        result = re.match(pattern, raw_response_header)
+        self.http_response_code = int(result.group(1))
+
+    def __set_headers_from_string(self, raw_headers):
+        # Normalize line breaks
+        raw_headers = raw_headers.replace("\r\n", "\n")
+        # There will be multiple headers if a 301 was followed
+        # or a proxy was followed, etc
+        header_collection = raw_headers.strip().split("\n\n")
+        # We just want the last response(at the end)
+        raw_header = header_collection[-1]
+        header_components = raw_header.split("\n")
+        for line in header_components:
+            if ':' not in line:
+                self.set_http_response_code_from_header(line)
+            else:
+                idx = line.index(': ')
+                key = line[:idx].strip()
+                value = line[idx + 2:].strip()
+                self.headers.update({
+                    key: value
+                })
---
+++ tests/http/graph_raw_response_test.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import unittest
+
+from python_facebook.sdk.http.graph_raw_response import GraphRawResponse
+
+
+
+class GraphRawResponseTest(unittest.TestCase):
+
+    fakeRawProxyHeader = """HTTP/1.0 200 Connection established
+    Proxy-agent: Kerio Control/7.1.1 build 1971\r\n\r\n"""
+
+    fakeRawHeader = """HTTP/1.1 200 OK
+    Etag: "9d86b21aa74d74e574bbb35ba13524a52deb96e3"
+    Content-Type: text/javascript; charset=UTF-8
+    X-FB-Rev: 9244768
+    Date: Mon, 19 May 2014 18:37:17 GMT
+    X-FB-Debug: 02QQiffE7JG2rV6i/Agzd0gI2/OOQ2lk5UW0=
+    Access-Control-Allow-Origin: *\r\n\r\n"""
+
+    fakeHeadersAsArray = {
+        'Etag': '"9d86b21aa74d74e574bbb35ba13524a52deb96e3"',
+        'Content-Type': 'text/javascript; charset=UTF-8',
+        'X-FB-Rev': '9244768',
+        'Date': 'Mon, 19 May 2014 18:37:17 GMT',
+        'X-FB-Debug': '02QQiffE7JG2rV6i/Agzd0gI2/OOQ2lk5UW0=',
+        'Access-Control-Allow-Origin': '*',
+    }
+
+    jsonFakeHeader = 'x-fb-ads-insights-throttle: {"app_id_util_pct": 0.00,"acc_id_util_pct": 0.00}'
+    jsonFakeHeaderAsArray = {'x-fb-ads-insights-throttle': '{"app_id_util_pct": 0.00,"acc_id_util_pct": 0.00}'}
+
+    def testCanSetTheHeadersFromAnArray(self):
+        myHeaders = {
+            'foo': 'bar',
+            'baz': 'faz',
+        }
+        response = GraphRawResponse(myHeaders, '')
+        headers = response.get_headers()
+        self.assertEqual(myHeaders, headers)
+
+    def testCanSetTheHeadersFromAString(self):
+        response = GraphRawResponse(self.fakeRawHeader, '')
+        headers = response.get_headers()
+        httpResponseCode = response.get_http_response_code()
+        self.assertEqual(self.fakeHeadersAsArray, headers)
+        self.assertEqual(200, httpResponseCode)
+
+    def testWillIgnoreProxyHeaders(self):
+        response = GraphRawResponse(self.fakeRawProxyHeader + self.fakeRawHeader, '')
+        headers = response.get_headers()
+        httpResponseCode = response.get_http_response_code()
+        self.assertEqual(self.fakeHeadersAsArray, headers)
+        self.assertEqual(200, httpResponseCode)
+
+    def testCanTransformJsonHeaderValues(self):
+        response = GraphRawResponse(self.jsonFakeHeader, '')
+        headers = response.get_headers()
+        self.assertEqual(self.jsonFakeHeaderAsArray['x-fb-ads-insights-throttle'], headers[
+            'x-fb-ads-insights-throttle'])
