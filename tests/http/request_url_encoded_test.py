74838d187a5022d50307102f958112d0ea279b4e
shostelet <simon.hostelet@gmail.com>
2017-07-19 15:36:01 +0200

    Uses OrderedDict to prevent false negative on tests

--- tests/http/request_url_encoded_test.py
+++ tests/http/request_url_encoded_test.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from collections import OrderedDict
+
 from tests import PythonFacebookTestCase
 from python_facebook.sdk.http.request_body_url_encoded import RequestBodyUrlEncoded
 
@@ -8,15 +10,15 @@ from python_facebook.sdk.http.request_body_url_encoded import RequestBodyUrlEnco
 class RequestUrlEncodedTest(PythonFacebookTestCase):
 
     def testCanProperlyEncodeAnArrayOfParams(self):
-        message = RequestBodyUrlEncoded({
+        message = RequestBodyUrlEncoded(OrderedDict({
             'foo': 'bar',
             'scawy_vawues': '@FooBar is a real twitter handle.',
-        })
+        }))
         body = message.get_body()
         self.assertEqual('foo=bar&scawy_vawues=%40FooBar+is+a+real+twitter+handle.', body)
 
     def testSupportsMultidimensionalParams(self):
-        message = RequestBodyUrlEncoded({
+        message = RequestBodyUrlEncoded(OrderedDict({
             'faz': [1, 2, 3],
             'foo': 'bar',
             'targeting': {
@@ -32,7 +34,7 @@ class RequestUrlEncodedTest(PythonFacebookTestCase):
                     },
                 },
             },
-        })
+        }))
         body = message.get_body()
         self.assertEqual(
             'faz%5B0%5D=1&faz%5B1%5D=2&faz%5B2%5D=3&foo=bar&targeting%5Bcountries%5D=US%2CGB&targeting%5Bage_min%5D=13&call_to_action%5Btype%5D=LEARN_MORE&call_to_action%5Bvalue%5D%5Blink%5D=http%3A%2F%2Fexample.com&call_to_action%5Bvalue%5D%5Bsponsorship%5D%5Bimage%5D=http%3A%2F%2Fexample.com%2Fbar.jpg', body)
