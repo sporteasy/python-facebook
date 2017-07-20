e3db906cd2e05bcd485ffa2ffb021a73729988a7
Karl <karl.wnw@gmail.com>
2017-07-20 18:38:53 +0200

    Remove obsolete files

--- python_facebook/settings.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import os
-
-
-DEFAULT_APP_ID = os.environ.get('FACEBOOK_APP_ID')
-
-DEFAULT_APP_SECRET = os.environ.get('FACEBOOK_APP_SECRET')
-
-USE_APP_SECRET_PROOF = True
